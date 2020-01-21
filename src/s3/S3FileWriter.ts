bbc99cc7a6e2cdba716e94e72e170cbccec09018
Yoshihiro Seki <nikochan2k@gmail.com>
2020-01-22 02:49:20 +0900

    remove unnecessary interface

--- src/s3/S3FileWriter.ts
+++ src/s3/S3FileWriter.ts
@@ -1,9 +1,8 @@
-import { AbstractFileWriter, FileWriter } from "kura";
+import { AbstractFileWriter } from "kura";
 import { S3Accessor } from "./S3Accessor";
 import { S3FileEntry } from "./S3FileEntry";
 
-export class S3FileWriter extends AbstractFileWriter<S3Accessor>
-  implements FileWriter {
+export class S3FileWriter extends AbstractFileWriter<S3Accessor> {
   constructor(fileEntry: S3FileEntry, file: File) {
     super(fileEntry, file);
   }
