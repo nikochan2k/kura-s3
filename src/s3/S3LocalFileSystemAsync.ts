473cf8c3f94236ab77788b8405005a7b37ddc2b7
関 慶宏 <seki@ifx.local>
2020-01-28 08:07:26 +0900

    Add overroads with S3LocalFileSystemAsync

--- src/s3/S3LocalFileSystemAsync.ts
+++ src/s3/S3LocalFileSystemAsync.ts
@@ -1,13 +1,20 @@
-import { LocalFileSystemAsync } from "kura";
+import { LocalFileSystemAsync, Permission } from "kura";
 import { S3 } from "aws-sdk";
 import { S3LocalFileSystem } from "./S3LocalFileSystem";
 
 export class S3LocalFileSystemAsync extends LocalFileSystemAsync {
+  constructor(config: S3.ClientConfiguration, bucket: string);
   constructor(
     config: S3.ClientConfiguration,
     bucket: string,
-    useIndex = false
-  ) {
-    super(new S3LocalFileSystem(config, bucket, useIndex));
+    useIndex: boolean
+  );
+  constructor(
+    config: S3.ClientConfiguration,
+    bucket: string,
+    permission: Permission
+  );
+  constructor(config: S3.ClientConfiguration, bucket: string, value?: any) {
+    super(new S3LocalFileSystem(config, bucket, value));
   }
 }
