034973e0e9bb74dd415f85964099a04dae56d78c
Yoshihiro Seki <nikochan2k@gmail.com>
2020-05-10 15:45:19 +0900

    update packages

--- src/s3/S3Accessor.ts
+++ src/s3/S3Accessor.ts
@@ -134,13 +134,6 @@ export class S3Accessor extends AbstractAccessor {
     return this.doPutContentToS3(fullPath, blob);
   }
 
-  protected initialize(options: FileSystemOptions) {
-    if (options.shared == null) {
-      options.shared = true;
-    }
-    super.initialize(options);
-  }
-
   private async doGetContentUsingGetObject(fullPath: string) {
     try {
       const path = normalizePath(this.rootDir + DIR_SEPARATOR + fullPath);
