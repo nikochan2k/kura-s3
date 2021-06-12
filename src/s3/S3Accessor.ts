6ffc189d32dbab4c79e5ec9b80e08d248a3d3037
Yoshihiro Seki <nikochan2k@gmail.com>
2021-06-12 11:45:23 +0900

    rollback

--- src/s3/S3Accessor.ts
+++ src/s3/S3Accessor.ts
@@ -1,5 +1,5 @@
 import { AWSError } from "aws-sdk";
-import {
+import S3, {
   ClientConfiguration,
   CompletedMultipartUpload,
   CompleteMultipartUploadRequest,
@@ -49,18 +49,12 @@ const hasBuffer = typeof Buffer === "function";
 
 const hasBlob = typeof Blob === "function";
 
-let AWS: any;
-if (isReactNative) {
-  AWS = require("aws-sdk/dist/aws-sdk-react-native");
-} else {
-  AWS = require("aws-sdk");
-}
 export class S3Accessor extends AbstractAccessor {
   // #region Properties (3)
 
   public filesystem: FileSystem;
   public name: string;
-  public s3: any;
+  public s3: S3;
 
   // #endregion Properties (3)
 
@@ -82,7 +76,7 @@ export class S3Accessor extends AbstractAccessor {
       config.httpOptions.connectTimeout = 1000;
     }
     config.signatureVersion = "v4";
-    this.s3 = new AWS.S3(config);
+    this.s3 = new S3(config);
     this.filesystem = new S3FileSystem(this);
     if (!this.rootDir.startsWith(DIR_SEPARATOR)) {
       this.rootDir = DIR_SEPARATOR + this.rootDir;
